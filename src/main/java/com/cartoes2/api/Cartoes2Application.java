91e9b4cd5c63a807e7bd0bf56c9062f81e3287e1
MatheusMaroca <matheusbarrosomaroca@hotmail.com>
2020-09-15 15:48:30 -0300

    Arquivos iniciais da aplicacao

---
+++ src/main/java/com/cartoes2/api/Cartoes2Application.java
@@ -0,0 +1,13 @@
+package com.cartoes2.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cartoes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cartoes2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartoes2/api/Cartoes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartoes2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cartoes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
